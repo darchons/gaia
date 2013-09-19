83639ac3a69021df04477ca3af184c92c423a4d0
Jim Chen <jimnchen@gmail.com>
2013-09-19 11:18:12 -0400

    Update ua-override-prefs.js

--- build/ua-override-prefs.js
+++ build/ua-override-prefs.js
@@ -136,3 +136,4 @@ pref("general.useragent.override.univision.com", "\\(Mobile#(Android; Mobile"); 
 pref("general.useragent.override.redstarbelgrade.info", "\\(Mobile#(Android; Mobile"); // bug 878653
 pref("general.useragent.override.vesti-online.com", "\\(Mobile#(Android; Mobile"); // bug 878655
 pref("general.useragent.override.example.com", "\\(Mobile#(Android; Mobile"); // bug xxxxxx
+pref("general.useragent.override.example.org", "\\(Mobile#(Android; Mobile"); // bug xxxxxx
